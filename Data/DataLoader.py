7aa5995a02f80e3ac84ca50c35bc4e658f463725
Zyno <agirzel@gmail.com>
2023-05-19 19:33:33 +0200

    dataloader fixing and model experimentation

--- Data/DataLoader.py
+++ Data/DataLoader.py
@@ -3,27 +3,40 @@ import torch
 import torchvision
 import pandas as pd
 
+class MyDataset(torch.utils.data.Dataset):
+    def __init__(self, subset, transform=None):
+        self.subset = subset
+        self.transform = transform
+    def __getitem__(self, index):
+        x, y = self.subset[index]
+        if self.transform:
+            x = self.transform(x)
+        return x, y
+    def __len__(self):
+        return len(self.subset)
+
 def DogsDatasetDataloders(transformer, batch_size, num_workers, shuffle=True):
     #activate cuda for performance enhacement
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
     print("Using: ", device)
 
     #loading data
+    # loading data
     dataset = torchvision.datasets.ImageFolder('./Dog-Breed-classif/train')
     labels = pd.read_csv("./Dog-Breed-classif/labels.csv")
 
-    #splitting data
+    # splitting data
     dataset_len = labels.shape[0]
     indexes = list(range(dataset_len))
     random.shuffle(indexes)
-    train_indexes = indexes[0:int(dataset_len*0.7)]
-    validation_indexes = indexes[int(dataset_len*0.7)+1:dataset_len-1]
+    train_indexes = indexes[0:int(dataset_len * 0.7)]
+    validation_indexes = indexes[int(dataset_len * 0.7) + 1:dataset_len - 1]
     train_subset = torch.utils.data.Subset(dataset, train_indexes)
-    train_subset.dataset.transforms = transformer['train']
+    train_dataset = MyDataset(train_subset, transform=transformer["train"])
     validation_subset = torch.utils.data.Subset(dataset, validation_indexes)
-    validation_subset.dataset.transforms = transformer['val']
+    validation_dataset = MyDataset(validation_subset, transform=transformer["val"])
 
-    dataloaders_dict = {"train": torch.utils.data.DataLoader(train_subset, batch_size=batch_size, shuffle=shuffle, num_workers=num_workers),
-                        "val": torch.utils.data.DataLoader(validation_subset, batch_size=batch_size, shuffle=shuffle, num_workers=num_workers)}
+    dataloaders_dict = {"train": torch.utils.data.DataLoader(train_dataset, batch_size=batch_size, shuffle=shuffle, num_workers=num_workers),
+                        "val": torch.utils.data.DataLoader(validation_dataset, batch_size=batch_size, shuffle=shuffle, num_workers=num_workers)}
 
     return dataloaders_dict
