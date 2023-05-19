ccc78b11db9619d44a3d424306e0e26a9a6fd3b2
Zyno <agirzel@gmail.com>
2023-05-19 18:39:00 +0200

    Fixing transforms for each subset

--- Data/DataLoader.py
+++ Data/DataLoader.py
@@ -3,13 +3,13 @@ import torch
 import torchvision
 import pandas as pd
 
-def DogsDatasetDataloders(batch_size, num_workers, shuffle=True):
+def DogsDatasetDataloders(transformer, batch_size, num_workers, shuffle=True):
     #activate cuda for performance enhacement
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
     print("Using: ", device)
 
     #loading data
-    dataset = torchvision.datasets.ImageFolder('../Dog-Breed-classif/train', torchvision.transforms.ToTensor())
+    dataset = torchvision.datasets.ImageFolder('../Dog-Breed-classif/train')
     labels = pd.read_csv("../Dog-Breed-classif/labels.csv")
 
     #splitting data
@@ -19,9 +19,11 @@ def DogsDatasetDataloders(batch_size, num_workers, shuffle=True):
     train_indexes = indexes[0:int(dataset_len*0.7)]
     validation_indexes = indexes[int(dataset_len*0.7)+1:dataset_len-1]
     train_subset = torch.utils.data.Subset(dataset, train_indexes)
+    train_subset.dataset.transforms = transformer['train']
     validation_subset = torch.utils.data.Subset(dataset, validation_indexes)
+    validation_subset.dataset.transforms = transformer['val']
 
-    dataloaders_dict={"train": torch.utils.data.DataLoader(train_subset, batch_size=batch_size, shuffle=shuffle, num_workers=num_workers),
-                      "val": torch.utils.data.DataLoader(validation_subset, batch_size=batch_size, shuffle=shuffle, num_workers=num_workers)}
+    dataloaders_dict = {"train": torch.utils.data.DataLoader(train_subset, batch_size=batch_size, shuffle=shuffle, num_workers=num_workers),
+                        "val": torch.utils.data.DataLoader(validation_subset, batch_size=batch_size, shuffle=shuffle, num_workers=num_workers)}
 
     return dataloaders_dict
