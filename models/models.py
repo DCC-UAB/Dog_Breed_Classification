bf17009cb89ef6028901c34c1be6e48c51a1b96d
Zynokrex <81986384+Zynokrex@users.noreply.github.com>
2023-05-22 00:57:42 +0200

    New models

--- models/models.py
+++ models/models.py
@@ -24,5 +24,33 @@ def initialize_model_regnet_x_16(num_classes):
 
     return model, input_size
 
+def initialize_model_regnet_x_16(num_classes):
+    # Resnet18
+    model = torchvision.models.regnet_x_16gf(pretrained=True)
+    set_parameter_requires_grad(model, True)
+    model.fc = nn.Linear(2048, num_classes)
+    input_size = 224
+
+    return model, input_size
+
+def initialize_model_regnet_x_16(num_classes):
+    # Resnet18
+    model = torchvision.models.regnet_x_16gf(pretrained=True)
+    set_parameter_requires_grad(model, True)
+    model.fc = nn.Linear(2048, num_classes)
+    input_size = 224
+
+    return model, input_size
+
+
+def initialize_model_regnet_x_16(num_classes):
+    # Resnet18
+    model = torchvision.models.regnet_x_16gf(pretrained=True)
+    set_parameter_requires_grad(model, True)
+    model.fc = nn.Linear(2048, num_classes)
+    input_size = 224
+
+    return model, input_size
+
 
 
