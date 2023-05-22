c608b91998ed69823cd1f73cf1eca8f9c56b498e
Zynokrex <81986384+Zynokrex@users.noreply.github.com>
2023-05-22 11:59:53 +0200

    Test dataloader added

--- utils/DataLoader.py
+++ utils/DataLoader.py
@@ -3,15 +3,18 @@ import torch
 import torchvision
 import pandas as pd
 
+
 class MyDataset(torch.utils.data.Dataset):
     def __init__(self, subset, transform=None):
         self.subset = subset
         self.transform = transform
+
     def __getitem__(self, index):
         x, y = self.subset[index]
         if self.transform:
             x = self.transform(x)
         return x, y
+
     def __len__(self):
         return len(self.subset)
 
@@ -30,15 +33,23 @@ def dogs_dataset_dataloders(transformer, batch_size, num_workers, dataset_path, 
     dataset_len = labels.shape[0]
     indexes = list(range(dataset_len))
     random.shuffle(indexes)
-    train_indexes = indexes[0:int(dataset_len * 0.7)]
-    validation_indexes = indexes[int(dataset_len * 0.7) + 1:dataset_len - 1]
+    train_indexes = indexes[0:int(dataset_len * 0.6)]
+    validation_indexes = indexes[int(dataset_len * 0.6) + 1: int(dataset_len * 0.9)]
+    test_indexes = indexes[int(dataset_len * 0.9) + 1: (dataset_len-1)]
     train_subset = torch.utils.data.Subset(dataset, train_indexes)
     train_dataset = MyDataset(train_subset, transform=transformer["train"])
     validation_subset = torch.utils.data.Subset(dataset, validation_indexes)
     validation_dataset = MyDataset(validation_subset, transform=transformer["val"])
+    test_subset = torch.utils.data.Subset(dataset, test_indexes)
+    test_dataset = MyDataset(test_subset, transform=transformer["test"])
 
     #creating dataloaders
-    dataloaders_dict = {"train": torch.utils.data.DataLoader(train_dataset, batch_size=batch_size, shuffle=shuffle, num_workers=num_workers),
-                        "val": torch.utils.data.DataLoader(validation_dataset, batch_size=batch_size, shuffle=shuffle, num_workers=num_workers)}
+    dataloaders_dict = {"train": torch.utils.data.DataLoader(train_dataset, batch_size=batch_size,
+                                                             shuffle=shuffle, num_workers=num_workers),
+                        "val": torch.utils.data.DataLoader(validation_dataset, batch_size=batch_size,
+                                                           shuffle=shuffle, num_workers=num_workers),
+                        "test": torch.utils.data.DataLoader(test_dataset, batch_size=batch_size,
+                                                            shuffle=shuffle, num_workers=num_workers)
+                        }
 
     return dataloaders_dict
