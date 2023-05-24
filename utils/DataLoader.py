b275f95c8c1a89cfcaaf9a3de3fa0818dd3ad305
Sofia Di Capua <73697639+SofiaDiCapua@users.noreply.github.com>
2023-05-24 12:50:38 +0200

    Commented DataLoader.py

--- utils/DataLoader.py
+++ utils/DataLoader.py
@@ -1,35 +1,42 @@
+# Imports
 import random
 import torch
 import torchvision
 import pandas as pd
 
+"""
+This Python code defines a custom Dataset class in PyTorch and a function to load, process, and split a dataset of dog images into 
+training, validation, and test sets. It also creates PyTorch dataloaders for each of these sets, which are used to efficiently 
+load the data during training of a neural network. The function uses CUDA for better performance if available.
+"""
 
+# Definition of a custom dataset class
 class MyDataset(torch.utils.data.Dataset):
     def __init__(self, subset, transform=None):
-        self.subset = subset
-        self.transform = transform
+        self.subset = subset  # Subset of data
+        self.transform = transform  # Data augmentation/transformations
 
     def __getitem__(self, index):
-        x, y = self.subset[index]
-        if self.transform:
-            x = self.transform(x)
+        x, y = self.subset[index]  # Get an item by index
+        if self.transform:  # If any transformations were passed
+            x = self.transform(x)  # Apply transformations
         return x, y
 
     def __len__(self):
-        return len(self.subset)
+        return len(self.subset)  # Return the length of subset
 
-
+# Function to load data and return dataloaders for each data subset
 def dogs_dataset_dataloders(transformer, batch_size, num_workers, dataset_path, shuffle=True):
 
-    #activate cuda for performance enhacement
+    # Use CUDA if available for performance enhancement
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
     print("Using: ", device)
 
-    #loading data
+    # Load data
     dataset = torchvision.datasets.ImageFolder(dataset_path+'/train')
     labels = pd.read_csv(dataset_path+'/labels.csv')
 
-    # splitting data
+    # Split data into training, validation and testing subsets
     dataset_len = labels.shape[0]
     indexes = list(range(dataset_len))
     random.shuffle(indexes)
@@ -43,7 +50,7 @@ def dogs_dataset_dataloders(transformer, batch_size, num_workers, dataset_path, 
     test_subset = torch.utils.data.Subset(dataset, test_indexes)
     test_dataset = MyDataset(test_subset, transform=transformer["test"])
 
-    #creating dataloaders
+    # Create dataloaders for each subset
     dataloaders_dict = {"train": torch.utils.data.DataLoader(train_dataset, batch_size=batch_size,
                                                              shuffle=shuffle, num_workers=num_workers),
                         "val": torch.utils.data.DataLoader(validation_dataset, batch_size=batch_size,
