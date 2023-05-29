981e69baa46c56f423cbcceb44091277af14e1e0
Zynokrex <81986384+Zynokrex@users.noreply.github.com>
2023-05-29 18:08:50 +0200

    Add files via upload

--- models/models.py
+++ models/models.py
@@ -9,26 +9,31 @@ network models, adjusts their final layer based on the required number of classe
 the model parameters for feature extraction.
 """
 
+
 # parameter extraction
 def set_parameter_requires_grad(model, feature_extracting):
     if feature_extracting:
         for param in model.parameters():
             param.requires_grad = False
 
+
 # Initialization of the ResNet 18 model
 def initialize_model_resnet_18(num_classes, pre_trained=True,
                                feat_extraction=True):
     # Resnet18
-    model = torchvision.models.resnet18(pretrained=pre_trained)
-    if (pre_trained):
+    if pre_trained:
+        weights = torchvision.models.ResNet18_Weights.IMAGENET1K_V1
+        model = torchvision.models.resnet18(weights=weights)
         model_name = "ResNet_18_PreTrained"
     else:
+        model = torchvision.models.resnet18()
         model_name = "ResNet_18"
     set_parameter_requires_grad(model, feat_extraction)
     model.fc = nn.Linear(512, num_classes)
     input_size = 224
 
     return model, input_size, model_name
+
 
 # Initialization of the ResNeXt-101_64x4d model
 def initialize_model_resnext101_64x4d(num_classes, pre_trained=True,
@@ -48,14 +53,17 @@ def initialize_model_resnext101_64x4d(num_classes, pre_trained=True,
 
     return model, input_size, model_name
 
+
 # Initialization of the ResNeXt-101_32x8d model
 def initialize_model_resnext101_32x8d(num_classes, pre_trained=True,
                                       feat_extraction=True):
     # ResNext101_32x8d
-    model = torchvision.models.resnext101_32x8d(pretrained=pre_trained)
-    if (pre_trained):
+    if pre_trained:
+        weights = torchvision.models.ResNeXt101_32X8D_Weights.IMAGENET1K_V1
+        model = torchvision.models.resnext101_32x8d(weights=weights)
         model_name = "ResNext101_32x8d_PreTrained"
     else:
+        model = torchvision.models.resnext101_32x8d()
         model_name = "ResNext101_32x8d"
     set_parameter_requires_grad(model, feat_extraction)
     model.fc = nn.Linear(2048, num_classes)
@@ -63,14 +71,17 @@ def initialize_model_resnext101_32x8d(num_classes, pre_trained=True,
 
     return model, input_size, model_name
 
+
 # Initialization of the ResNeXt-50_32x4d model
 def initialize_model_resnext50_32x4d(num_classes, pre_trained=True,
                                      feat_extraction=True):
     # ResNext101_32x8d
-    model = torchvision.models.resnext50_32x4d(pretrained=pre_trained)
-    if (pre_trained):
+    if pre_trained:
+        weights = torchvision.models.ResNeXt50_32X4D_Weights.IMAGENET1K_V1
+        model = torchvision.models.resnext101_64x4d(weights=weights)
         model_name = "ResNext50_32x4d_PreTrained"
     else:
+        model = torchvision.models.resnext50_32x4d()
         model_name = "ResNext50_32x4d"
     set_parameter_requires_grad(model, feat_extraction)
     model.fc = nn.Linear(2048, num_classes)
