8a9f3be5dfda96f22aa1d9cdc089a548e9307e35
Zynokrex <81986384+Zynokrex@users.noreply.github.com>
2023-05-29 18:34:03 +0200

    Documentation of test.py

--- utils/test.py
+++ utils/test.py
@@ -7,6 +7,27 @@ device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
 
 def test(model, dataloader):
+    """
+    Description
+    -----------
+    Does the testing phase of a model though the test dataloader to give more information about the model
+    that we have trained and then returns the information we got of this test.
+    
+    Parameters
+    ----------
+    model: class
+        Contains the trained model we want to test.
+    dataloader: dictionary of torch.utils.data.DataLoader
+        Contains the dataloader of the testing phase.
+        
+    Returns
+    -------
+    predictions: Pandas.DataFrame
+        Contains the original labels, the predicted ones and if the prediction was right.
+    accuracy: float
+        The computed accuracy of our test
+    """
+    
     model.eval()
     predictions = pd.DataFrame(columns=["label", "prediction"])
     with torch.no_grad():
@@ -26,6 +47,32 @@ def test(model, dataloader):
 
 
 def test_on_fold(model, test_loader, model_name="", optimizer_name="", load_weights=False):
+    """
+    Description
+    -----------
+    Joins the weight loading with the test function
+    
+    Parameters
+    ----------
+    model: class
+        Contains the trained model we want to test.
+    test_dataloader: dictionary of torch.utils.data.DataLoader
+        Contains the dataloaders of the testing phase.
+    model_name: str
+        The model name, used to open the weights file. By default "".
+    optimizer_name: str
+        The optimizer name, used to open the weights file. By default "".
+    load_weights: bool
+        If true loads the weights of a previously trained model. By default False.
+      
+    Returns
+    -------
+    predictions: Pandas.DataFrame
+        Contains the original labels, the predicted ones and if the prediction was right.
+    accuracy: float
+        The computed accuracy of our test
+    """
+        
     if load_weights:
         path = "./models/"+model_name+"_"+optimizer_name+".pth"
         model.load_state_dict(torch.load(path, device))
